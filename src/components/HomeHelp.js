441e3bf7fc42d0bf34e8b4a572b447e3e9fa1e57
Angelika Tyburska <52262666+AngieTy@users.noreply.github.com>
2019-12-19 18:38:29 +0100

    Modified pagination

--- src/components/HomeHelp.js
+++ src/components/HomeHelp.js
@@ -6,48 +6,175 @@ class HomeHelp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isClicked1: true,
-      isClicked2: false,
-      isClicked3: false
+      isFundationsClicked: true,
+      isOrganizationsClicked: false,
+      isLocalsClicked: false
     };
   }
 
-  handleToggleBtn1 = () => {
+  handleButtonFundations = () => {
     this.setState(state => {
       console.log("klik!");
-      console.log(this.state.isClicked1);
       return {
-        isClicked1: !state.isClicked1,
-        isClicked2: false,
-        isClicked3: false
+        isFundationsClicked: true,
+        isOrganizationsClicked: false,
+        isLocalsClicked: false
       };
     });
   };
-  handleToggleBtn2 = () => {
+  handleButtonOrganizations = () => {
     this.setState(state => {
       console.log("klik!");
-      console.log(this.state.isClicked2);
       return {
-        isClicked1: false,
-        isClicked2: !state.isClicked2,
-        isClicked3: false
+        isFundationsClicked: false,
+        isOrganizationsClicked: true,
+        isLocalsClicked: false
       };
     });
   };
-  handleToggleBtn3 = () => {
+  handleButtonLocals = () => {
     this.setState(state => {
       console.log("klik!");
-      console.log(this.state.isClicked3);
       return {
-        isClicked1: false,
-        isClicked2: false,
-        isClicked3: !state.isClicked3
+        isFundationsClicked: false,
+        isOrganizationsClicked: false,
+        isLocalsClicked: true
       };
     });
   };
 
   render() {
-    const { isClicked1, isClicked2, isClicked3 } = this.state;
+    const fundations = [
+      {
+        fundations: "Fundacja 'Dbam o Zdrowie'",
+        descriptions:
+          "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji finansowej.",
+        needs: "ubrania, jedzenie, sprzęt AGD, meble, zabawki"
+      },
+      {
+        fundations: "Fundacja 'Dla dzieci'",
+        descriptions: "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
+        needs: "ubrania, meble, zabawki"
+      },
+      {
+        fundations: "Fundacja 'Bez domu'",
+        descriptions:
+          "Cel i misja: Pomoc osobom nieposiadającym miejsca zamieszkania.",
+        needs: "ubrania, jedzenie, ciepłe koce"
+      },
+      {
+        fundations: "Fundacja 'Bez kota'",
+        descriptions:
+          "Cel i misja: Zwiększenie adopcji kotów przez silne, niezależne kobiety.",
+        needs: "kotki, kotki, kotki"
+      },
+      {
+        fundations: "Fundacja 'Dla mnie'",
+        descriptions:
+          "Cel i misja: Zbieranie pieniędzy i dóbr osobistych dla królowej zycia - mnie.",
+        needs: "wszystko"
+      },
+      {
+        fundations: "Fundacja 'Dla sadełka'",
+        descriptions:
+          "Cel i misja: wspomożenie wzrostu tkanki tłuszczowej u osób z sylwetką skinny-fat.",
+        needs: "piwo, pizza, burgerki"
+      },
+      {
+        fundations: "Fundacja 'Bez niczego'",
+        descriptions:
+          "Cel i misja: Wsparcie osób mających za dużo dóbr osobistych w pozbywaniu się ich.",
+        needs: "nic"
+      },
+      {
+        fundations: "Fundacja 'Ogól głowę'",
+        descriptions:
+          "Cel i misja: Golenie głów na łysą glace w celu pozbycia się włosów w odpływie prysznica.",
+        needs: "włosy, prysznic, nożyczki, krata"
+      },
+      {
+        fundations: "Fundacja 'Daj pizze'",
+        descriptions:
+          "Cel i misja: zwiększenie świadomości wśród nie-imprezowiczów o ważności karmienia osób na kacu pizzą.",
+        needs: "pizza, papryka, cebula, ser, ser, ser"
+      }
+    ];
+    const organizations = [
+      {
+        fundations: "Organizacja 'Niedziela'",
+        descriptions: "Cel i misja: Chodzenie do kościoła.",
+        needs: "różaniec, krzyż, sałata, woda święcona"
+      },
+      {
+        fundations: "Organizacja 'Bezchmurne niebo'",
+        descriptions: "Cel i misja: czyszczenie nieba z chmur.",
+        needs: "czyściciel chmur, szczotka, zmiotka"
+      },
+      {
+        fundations: "Organizacja 'Gwint'",
+        descriptions:
+          "Cel i misja: Zebranie całej talii kart do gwinta i ogranie Zoltana.",
+        needs: "karty, piwo, gorzałka, stół"
+      },
+      {
+        fundations: "Organizacja 'Bazyliszek'",
+        descriptions:
+          "Cel i misja: Ochrona zagrożonego bazyliszka srebrzystego.",
+        needs: "odszkodowanie, znaki ostrzegawcze, Wilga"
+      },
+      {
+        fundations: "Organizacja 'Czysty Gwint'",
+        descriptions:
+          "Cel i misja: Powstrzymanie hrabiego Monniera przed wprowadzeniem frakcji Skellige.",
+        needs: "pałki, cierpliwość, mocne drzwi"
+      },
+      {
+        fundations: "Organizacja 'Ochrona Olafa'",
+        descriptions:
+          "Cel i misja: Niedopuszczenie do śmierci Olafa - najlepszego zawodnika walk na pięści na Skellige.",
+        needs: "krzepa, siła do powalenia niedźwiedzia"
+      }
+    ];
+    const locals = [
+      {
+        fundations: "Zbiórka 'Usunąć tatuaż'",
+        descriptions:
+          "Cel i misja: Usunięcie tatuażu Geraltowi po pijackim wieczorze.",
+        needs: "magia, pleśń, zdrowy rozsądek"
+      },
+      {
+        fundations: "Zbiórka 'Budujemy pomnik'",
+        descriptions:
+          "Cel i misja: zebranie funduszów na budowę pomnika świętego Lebiody.",
+        needs: "pieniądze, narzędzia, surowiec"
+      },
+      {
+        fundations: "Zbiórka 'Odbudowa Kaer Morhen'",
+        descriptions:
+          "Cel i misja: Zebranie funduszy na odbudowę wiedźmińskiego siedliszcza.",
+        needs: "kupa złota, kupa kamienia, kupa ludzi"
+      }
+    ];
+    const {
+      isFundationsClicked,
+      isOrganizationsClicked,
+      isLocalsClicked
+    } = this.state;
+
+    let pagination;
+
+    if (isFundationsClicked) {
+      pagination = (
+        <HomeHelpPagination data={fundations} areClicked={this.state} />
+      );
+    } else if (isOrganizationsClicked) {
+      pagination = (
+        <HomeHelpPagination data={organizations} areClicked={this.state} />
+      );
+    } else {
+      pagination = <HomeHelpPagination data={locals} areClicked={this.state} />;
+    }
+
     return (
       <section className="home-help">
         <h2 className="home-help-header">Komu pomagamy</h2>
@@ -55,25 +182,22 @@ class HomeHelp extends Component {
         <div className="help-btns-box">
           <button
             className="help-btn"
-            onClick={this.handleToggleBtn1}
-            click={isClicked1}
-            // items={items1}
+            onClick={this.handleButtonFundations}
+            click={isFundationsClicked}
           >
             Fundacjom
           </button>
           <button
             className="help-btn"
-            onClick={this.handleToggleBtn2}
-            click={isClicked2}
-            // items={items2}
+            onClick={this.handleButtonOrganizations}
+            click={isOrganizationsClicked}
           >
             Organizacjom pozarządowym
           </button>
           <button
             className="help-btn"
-            onClick={this.handleToggleBtn3}
-            click={isClicked3}
-            // items={items3}
+            onClick={this.handleButtonLocals}
+            click={isLocalsClicked}
           >
             Lokalnym zbiórkom
           </button>
@@ -83,7 +207,7 @@ class HomeHelp extends Component {
           współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i
           czego potrzebują.
         </p>
-        <HomeHelpPagination />
+        {pagination}
       </section>
     );
   }
--- src/components/HomeHelpPagination.js
+++ src/components/HomeHelpPagination.js
@@ -4,80 +4,33 @@ class HomeHelpPagination extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isClicked: true,
-      paginationElements: [
-        {
-          fundations: "Fundacja 'Dbam o Zdrowie'",
-          descriptions:
-            "Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji finansowej.",
-          needs: "ubrania, jedzenie, sprzęt AGD, meble, zabawki"
-        },
-        {
-          fundations: "Fundacja 'Dla dzieci'",
-          descriptions: "Cel i misja: Pomoc dzieciom z ubogich rodzin.",
-          needs: "ubrania, meble, zabawki"
-        },
-        {
-          fundations: "Fundacja 'Bez domu'",
-          descriptions:
-            "Cel i misja: Pomoc osobom nieposiadającym miejsca zamieszkania.",
-          needs: "ubrania, jedzenie, ciepłe koce"
-        },
-        {
-          fundations: "Fundacja 'Bez kota'",
-          descriptions:
-            "Cel i misja: Zwiększenie adopcji kotów przez silne, niezależne kobiety.",
-          needs: "kotki, kotki, kotki"
-        },
-        {
-          fundations: "Fundacja 'Dla mnie'",
-          descriptions:
-            "Cel i misja: Zbieranie pieniędzy i dóbr osobistych dla królowej zycia - mnie.",
-          needs: "wszystko"
-        },
-        {
-          fundations: "Fundacja 'Dla sadełka'",
-          descriptions:
-            "Cel i misja: wspomożenie wzrostu tkanki tłuszczowej u osób z sylwetką skinny-fat.",
-          needs: "piwo, pizza, burgerki"
-        },
-        {
-          fundations: "Fundacja 'Bez niczego'",
-          descriptions:
-            "Cel i misja: Wsparcie osób mających za dużo dóbr osobistych w pozbywaniu się ich.",
-          needs: "nic"
-        },
-        {
-          fundations: "Fundacja 'Ogól głowę'",
-          descriptions:
-            "Cel i misja: Golenie głów na łysą glace w celu pozbycia się włosów w odpływie prysznica.",
-          needs: "włosy, prysznic, nożyczki, krata"
-        },
-        {
-          fundations: "Fundacja 'Daj pizze'",
-          descriptions:
-            "Cel i misja: zwiększenie świadomości wśród nie-imprezowiczów o ważności karmienia osób na kacu pizzą.",
-          needs: "pizza, papryka, cebula, ser, ser, ser"
-        }
-      ],
       currentPage: 1,
       itemsPerPage: 3
     };
   }
 
   handleClick = (event, i) => {
-    this.setState({
-      currentPage: i
-    });
+    if (
+      this.props.areClicked.isFundationsClicked === true ||
+      this.props.areClicked.isOrganizationsClicked === true ||
+      this.props.areClicked.isLocalsClicked === true
+    ) {
+      this.setState({
+        currentPage: 1
+      });
+    } else {
+      this.setState({
+        currentPage: i
+      });
+    }
   };
 
   render() {
-    const { paginationElements, currentPage, itemsPerPage } = this.state;
+    const { currentPage, itemsPerPage } = this.state;
 
     const indexOfLast = currentPage * itemsPerPage;
     const indexOfFirst = indexOfLast - itemsPerPage;
-    const currentNames = paginationElements.slice(indexOfFirst, indexOfLast);
-
+    const currentNames = this.props.data.slice(indexOfFirst, indexOfLast);
     const elements = currentNames.map((fundation, i) => {
       return (
         <div key={i} className="pagination-element">
@@ -90,21 +43,26 @@ class HomeHelpPagination extends Component {
       );
     });
     const pageNumbers = [];
+    let element;
     for (
       let i = 1;
-      i <= Math.ceil(paginationElements.length / itemsPerPage);
+      i <= Math.ceil(this.props.data.length / itemsPerPage);
       i++
     ) {
-      const element = (
-        <span
-          className="pagination-page"
-          key={i}
-          onClick={e => this.handleClick(e, i)}
-        >
-          {i}
-        </span>
-      );
-      pageNumbers.push(element);
+      if (this.props.data.length === 3) {
+        element = "";
+      } else {
+        element = (
+          <span
+            className="pagination-page"
+            key={i}
+            onClick={e => this.handleClick(e, i)}
+          >
+            {i}
+          </span>
+        );
+        pageNumbers.push(element);
+      }
     }
 
     return (
