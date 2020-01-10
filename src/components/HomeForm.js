31c54e8d7d2f0cd07cfcd2a8dabc6795bcb2d2ab
Angela <angelikatyburska@gmail.com>
2020-01-10 15:16:55 +0100

    Fixed form validation

--- src/components/HomeForm.js
+++ src/components/HomeForm.js
@@ -3,14 +3,13 @@ import ImgDeco from "../assets/assets/Decoration.svg";
 
 class HomeForm extends Component {
   state = {
+    isLogged: false,
     name: "",
     email: "",
     textarea: "",
-    errors: {
-      name: "",
-      email: "",
-      textarea: ""
-    }
+    errorName: "",
+    errorEmail: "",
+    errorTextarea: ""
   };
 
   handleChange = e => {
@@ -22,82 +21,111 @@ class HomeForm extends Component {
   handleSubmit = e => {
     e.preventDefault();
 
+    const url = "https://fer-api.coderslab.pl/v1/portfolio/contact";
+    const data = {
+      name: this.state.name,
+      email: this.state.email,
+      textarea: this.state.textarea
+    };
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+        // "X-Auth-Token": "20200108224609"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => {
+        if (response.ok === true) {
+          console.log(response);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    const {
+      name,
+      email,
+      textarea,
+      errorName,
+      errorEmail,
+      errorTextarea
+    } = this.state;
+
     const validateEmail = email => {
       const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       return re.test(String(email).toLowerCase());
     };
 
-    const { name, email, textarea } = this.state;
-
     if (name.length < 1) {
-      this.setState({
-        errors: {
-          name: "Pole nie może być puste."
-        }
-      });
+      this.setState({ errorName: "Pole nie może być puste." });
+
       console.log("sukces");
-    } else if (!validateEmail(email)) {
+    } else {
       this.setState({
-        errors: {
-          email: "Email nie jest poprawny."
-        }
+        errorName: ""
       });
+    }
+
+    if (validateEmail(email) === false) {
+      this.setState({ errorEmail: "Email nie jest poprawny." });
+
       console.log("sukces2");
-    } else if (textarea.length < 120) {
+    } else {
       this.setState({
-        errors: {
-          textarea: "Wiadomość musi mieć conajmniej 120 znaków."
-        }
+        errorEmail: ""
+      });
+    }
+    if (textarea.length < 120) {
+      this.setState({
+        errorTextarea: "Wiadomość powinna mieć minimum 120 znaków."
       });
 
       console.log("sukces3");
-      // console.log(validateEmail(this.state.errors));
-      // console.log(this.state.errors);
-    } else if (
-      name.length === 0 &&
-      email.length === 0 &&
-      textarea.length === 0
-    ) {
-      console.log("nie ma wysylania pustego formularza!");
     } else {
       this.setState({
-        errors: {
-          name: "",
-          email: "",
-          password: ""
-        }
+        errorTextarea: ""
       });
-      const url = "https://fer-api.coderslab.pl/v1/portfolio/contact";
-      const data = {
-        name: this.state.name,
-        email: this.state.email,
-        textarea: this.state.textarea
-      };
+    }
 
-      fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-          // "X-Auth-Token": "20200108224609"
-        },
-        body: JSON.stringify(data)
-      })
-        .then(resp => {
-          if (resp.ok === true) {
-            console.log(resp);
-          }
-        })
-        .catch(err => console.log(err));
+    if (
+      name.length > 1 &&
+      validateEmail(email) === true &&
+      textarea.length > 120
+    ) {
+      this.setState({
+        name: "",
+        email: "",
+        textarea: "",
+        isLogged: true,
+        errorName: "",
+        errorEmail: "",
+        errorTextarea: ""
+      });
+      // return true;
     }
   };
 
   render() {
-    const { name, email, textarea, errors } = this.state;
+    const {
+      name,
+      email,
+      textarea,
+      errorName,
+      errorEmail,
+      errorTextarea
+    } = this.state;
     return (
       <section className="home-form" id={this.props.name}>
         <div className="home-form-container">
           <div className="home-opacity-modifier clearfix">
-            <form className="home-form-form" onSubmit={this.handleSubmit}>
+            <form
+              className="home-form-form"
+              onSubmit={this.handleSubmit}
+              noValidate
+            >
               <h2 className="form-header">Skontaktuj się z nami</h2>
               <img src={ImgDeco} alt="image_decoration" className="form-img" />
               <div className="form-inputs-box">
@@ -110,7 +138,7 @@ class HomeForm extends Component {
                     placeholder="Angelika"
                     onChange={this.handleChange}
                   />
-                  {<span className="error">{errors.name}</span>}
+                  {<span className="error">{errorName}</span>}
                 </label>
 
                 <label className="form-input-title">
@@ -122,7 +150,7 @@ class HomeForm extends Component {
                     placeholder="abc@xyz.pl"
                     onChange={this.handleChange}
                   />
-                  {<span className="error">{errors.email}</span>}
+                  {<span className="error">{errorEmail}</span>}
                 </label>
               </div>
               <div className="form-text-box">
@@ -134,7 +162,7 @@ class HomeForm extends Component {
                     value={textarea}
                     onChange={this.handleChange}
                   />
-                  {<span className="error">{errors.textarea}</span>}
+                  {<span className="error">{errorTextarea}</span>}
                 </label>
               </div>
               <button className="form-button">Wyślij</button>
