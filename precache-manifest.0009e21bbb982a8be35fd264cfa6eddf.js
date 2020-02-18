3377063acbbe71623bceeb399cd807bb78ba1a94
Angela <angelikatyburska@gmail.com>
2020-02-18 20:21:56 +0100

    Updates

---
+++ precache-manifest.0009e21bbb982a8be35fd264cfa6eddf.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe75520d9d46a667813c7e955862cca",
+    "url": "/GiveThingsBack/index.html"
+  },
+  {
+    "revision": "e5fea2f942d3ee2863cb",
+    "url": "/GiveThingsBack/static/css/main.76a1c31c.chunk.css"
+  },
+  {
+    "revision": "0bb4331e453406cfba1c",
+    "url": "/GiveThingsBack/static/js/2.105fe4d4.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/GiveThingsBack/static/js/2.105fe4d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5fea2f942d3ee2863cb",
+    "url": "/GiveThingsBack/static/js/main.f1832f60.chunk.js"
+  },
+  {
+    "revision": "f04add61eb6259257807",
+    "url": "/GiveThingsBack/static/js/runtime-main.cc4cf30c.js"
+  },
+  {
+    "revision": "7db36abd712c228ea96e6206440a9eb3",
+    "url": "/GiveThingsBack/static/media/3_cols_background.7db36abd.png"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/GiveThingsBack/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "aab5aa4cf97893341cd5aed84c736cd5",
+    "url": "/GiveThingsBack/static/media/Background-Form.aab5aa4c.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/GiveThingsBack/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/GiveThingsBack/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "3bee335ab47afd8b714411f4841f0185",
+    "url": "/GiveThingsBack/static/media/Form-Hero-Image.3bee335a.jpg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/GiveThingsBack/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/GiveThingsBack/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/GiveThingsBack/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/GiveThingsBack/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/GiveThingsBack/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "c470ec0c56e643f8ee1898ea962e753a",
+    "url": "/GiveThingsBack/static/media/Icon-Arrow-Down.c470ec0c.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/GiveThingsBack/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/GiveThingsBack/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/GiveThingsBack/static/media/Signature.e54cfcfc.svg"
+  }
+]);
